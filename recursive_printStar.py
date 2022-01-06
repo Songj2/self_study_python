6c4ef76d8361e80dd002eca0b26f09e2c145799f
Songj2 <55727461+Songj2@users.noreply.github.com>
2022-01-06 18:35:32 +0900

    Update_recursive_printStar_success9
    9x9 출력
    * 억지로 조건을 줘서 성공
    * 27은 실패
    *  다음엔, n/3일때 모양 저장해서 회전하면서 적용하는 방식으로 도전

--- recursive_printStar.py
+++ recursive_printStar.py
@@ -3,28 +3,20 @@ list=[["*" for j in range(n)] for i in range(n)]
 
 def printStar(row, col, list, way):
     max= len(list)    
-    temp= 3
+    temp= 0
     if row<max and col<max:
-        while temp<= max:
-            print("temp/3: ", temp/3, "row: ", row, "col:", col, "temp/3*2: ", (temp/3)*2)
-            if temp/3<=row<(temp/3)*2 and temp/3<=col<(temp/3)*2:
-                list[row][col]= " "
-                
+        while temp<= max*3:
+            if max/3<=row<(max/3)*2 and max/3<=col<(max/3)*2:
+                list[row][col]= " "           
             elif way== 1:
-                if temp/3<=col<(temp/3)*2 and col%3==1 and row%3==1: #and row조건
-                    list[row][col]= " "
-                    print("COLUMN")
-            elif way== 2:
-                if temp/3<=row<(temp/3)*2 and row% 3==1 and col%3 == 1 : #and col조건
-                    list[row][col]= " "
-                    print("ROW")
-    
-            temp*=3 
+                if temp<col<=temp+2  and col%3==1 and row%3==1: 
+                    list[row][col]= " "    
+            temp+=3 
+            
         if row<max and col== max-1:
             return printStar(row+1, 0, list, 2)
         else:
-            return printStar(row, col+1, list, 1)
-        
+            return printStar(row, col+1, list, 1)        
     else:
         return
 
